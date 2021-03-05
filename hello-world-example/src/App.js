38ae6df5f4a4e85eb663fc1a1188397151e884cd
Jake Meinershagen <jakemeinershagen@gmail.com>
2021-03-04 20:17:50 -0600

    add save draft and finalize buttons to edit form[C

--- hello-world-example/src/App.js
+++ hello-world-example/src/App.js
@@ -10,23 +10,27 @@ function editButton(e){
   editCount++;
   const newElement = (
     <form id="profForm">
-        <button onClick={editButton}>Edit</button>
-        <button onClick={addButton}>Add New</button>
-        <p id="profID">
-          Prof ID:
-          <input type="text"></input>
-        </p>
-        <p id="profName">
-          Prof Name:
-          <input type="text"></input>
-        </p>
-        <p id="isDraft">Is Draft: {}</p>
-        <p id="profAvailability">Availability: {}</p>
-        <p id="profTeachableCourses">Teachable Courses: {}</p>
-        <p id="profPrefCourses">Preffered Courses: {}</p>
-        <p id="profCurrSchedule">Current Schedule: {}</p>
-        <p id="profNotes">Notes: {}</p>
-      </form>
+      <button onClick={editButton}>Edit</button>
+      <button onClick={addButton}>Add New</button>
+      <p id="profID">
+        Prof ID:
+        <input type="text"></input>
+      </p>
+      <p id="profName">
+        Prof Name:
+        <input type="text"></input>
+      </p>
+      <p id="isDraft">Is Draft: {}</p>
+      <p id="profAvailability">Availability: {}</p>
+      <p id="profTeachableCourses">Teachable Courses: {}</p>
+      <p id="profPrefCourses">Preffered Courses: {}</p>
+      <p id="profCurrSchedule">Current Schedule: {}</p>
+      <p id="profNotes">Notes:</p>
+      <textarea></textarea>
+
+      <button>Save Draft</button>
+      <button>Finalize</button>
+    </form>
   );
   ReactDOM.render(newElement, document.getElementById("profForm"));
   e.preventDefault();
@@ -41,18 +45,20 @@ function App() {
   return (
     <div className="App">
       <h1>Professor Info</h1>
-      <form id="profForm">
-        <button onClick={editButton}>Edit</button>
-        <button onClick={addButton}>Add New</button>
-        <p id="profID">Prof ID: {}</p>
-        <p id="profName">Prof Name: {}</p>
-        <p id="isDraft">Is Draft: {}</p>
-        <p id="profAvailability">Availability: {}</p>
-        <p id="profTeachableCourses">Teachable Courses: {}</p>
-        <p id="profPrefCourses">Preffered Courses: {}</p>
-        <p id="profCurrSchedule">Current Schedule: {}</p>
-        <p id="profNotes">Notes: {}</p>
-      </form>
+      <div id="profForm">
+        <form>
+          <button onClick={editButton}>Edit</button>
+          <button onClick={addButton}>Add New</button>
+          <p id="profID">Prof ID: {}</p>
+          <p id="profName">Prof Name: {}</p>
+          <p id="isDraft">Is Draft: {}</p>
+          <p id="profAvailability">Availability: {}</p>
+          <p id="profTeachableCourses">Teachable Courses: {}</p>
+          <p id="profPrefCourses">Preffered Courses: {}</p>
+          <p id="profCurrSchedule">Current Schedule: {}</p>
+          <p id="profNotes">Notes: {}</p>
+        </form>
+      </div>
     </div>
   );
 }
