01f7bb4c2fd6c4df7675c5a9686a137b5bd53a0d
Jake Meinershagen <jakemeinershagen@gmail.com>
2021-03-04 19:19:33 -0600

    Basic Prof Page

--- hello-world-example/src/App.js
+++ hello-world-example/src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Professor Info</h1>
+      <form>
+        <button>Edit</button>
+        <button>Add New</button>
+      </form>
+      <p>Prof ID: {}</p>
+      <p>Prof Name: {}</p>
+      <p>Is Draft: {}</p>
+      <p>Availability: {}</p>
+      <p>Teachable Courses: {}</p>
+      <p>Preffered Courses: {}</p>
+      <p>Current Schedule: {}</p>
+      <p>Notes: {}</p>
     </div>
   );
 }
