020ec77e05d70944dd0d03be787c0b70b0ecb18a
Jake Meinershagen <jakemeinershagen@gmail.com>
2021-03-04 20:04:13 -0600

    Adding some buttons and basic functionality

--- hello-world-example/src/App.js
+++ hello-world-example/src/App.js
@@ -1,22 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+
+var editCount = 0;
+
+function editButton(e){
+  console.log("edit function");
+  editCount++;
+  const newElement = (
+    <form id="profForm">
+        <button onClick={editButton}>Edit</button>
+        <button onClick={addButton}>Add New</button>
+        <p id="profID">
+          Prof ID:
+          <input type="text"></input>
+        </p>
+        <p id="profName">
+          Prof Name:
+          <input type="text"></input>
+        </p>
+        <p id="isDraft">Is Draft: {}</p>
+        <p id="profAvailability">Availability: {}</p>
+        <p id="profTeachableCourses">Teachable Courses: {}</p>
+        <p id="profPrefCourses">Preffered Courses: {}</p>
+        <p id="profCurrSchedule">Current Schedule: {}</p>
+        <p id="profNotes">Notes: {}</p>
+      </form>
+  );
+  ReactDOM.render(newElement, document.getElementById("profForm"));
+  e.preventDefault();
+}
+
+function addButton(e){
+  console.log("add function");
+  e.preventDefault();
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Professor Info</h1>
-      <form>
-        <button>Edit</button>
-        <button>Add New</button>
+      <form id="profForm">
+        <button onClick={editButton}>Edit</button>
+        <button onClick={addButton}>Add New</button>
+        <p id="profID">Prof ID: {}</p>
+        <p id="profName">Prof Name: {}</p>
+        <p id="isDraft">Is Draft: {}</p>
+        <p id="profAvailability">Availability: {}</p>
+        <p id="profTeachableCourses">Teachable Courses: {}</p>
+        <p id="profPrefCourses">Preffered Courses: {}</p>
+        <p id="profCurrSchedule">Current Schedule: {}</p>
+        <p id="profNotes">Notes: {}</p>
       </form>
-      <p>Prof ID: {}</p>
-      <p>Prof Name: {}</p>
-      <p>Is Draft: {}</p>
-      <p>Availability: {}</p>
-      <p>Teachable Courses: {}</p>
-      <p>Preffered Courses: {}</p>
-      <p>Current Schedule: {}</p>
-      <p>Notes: {}</p>
     </div>
   );
 }
