207642b43b2e5ab47c3710a40118a396abf1f9dc
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2021-04-09 14:00:19 -0500

    Refactored teacher.controller

--- hello-world-example/app/controllers/teacher.controller.js
+++ hello-world-example/app/controllers/teacher.controller.js
@@ -1,77 +1,88 @@
-const db = require("../models");
-const Teacher = db.teachers;
-const Op = db.Sequelize.Op;
+const {teachers: Teacher, Sequelize: {Op}} = require("../models");
 
-// Create and Save a new Teacher
-exports.create = (req, res) => {
-
-  // Validate request
-  if (!req.body.firstName) {
-    res.status(400).send({
-      message: "Content can not be empty!"
-    });
-    return;
-  }
-
-  // Create a teacger
-  const teacher = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    finalized: req.body.finalized ? req.body.finalized : false
-  };
-
-  // Save Tutorial in the database
-  Teacher.create(teacher)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
+module.exports = function(sequelize) {
+  // Create and Save a new Teacher
+  async function create(req, res) {
+    // Validate request
+    if (!req.body.firstName) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+  
+    // Create a teacger
+    const teacher = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      finalized: !!req.body.finalized
+    };
+  
+    try {
+      // Save Tutorial in the database
+      const data = await Teacher.create(teacher);
+      res.send(data)
+    } catch (err) {
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the teacher."
       });
-    });
-};
+    }
+  }
 
-// Retrieve all Teachers from the database.
-exports.findAll = (req, res) => {
-
-  const firstName = req.query.firstName;
-  var condition = firstName ? { firstName: { [Op.like]: `%${firstName}%` } } : null;
-
-  Teacher.findAll({ where: condition })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
+  // Retrieve all Teachers from the database.
+  async function findAll(req, res) {
+    const firstName = req.query.firstName;
+    let query = { where: null };
+    if (firstName) {
+      query.where = { 
+        firstName: { 
+          [Op.like]: `%${firstName}%` 
+        } 
+      };
+    }
+    try {
+      const data = await Teacher.create(query);
+      res.send(data)
+    } catch (err) {
       res.status(500).send({
         message:
           err.message || "Some error occurred while retrieving teachers."
       });
-    });
-};
+    }
+  }
+  // Find a single Teacher with an id
+  async function findOne(req, res) {
+    
+  }
 
-// Find a single Teacher with an id
-exports.findOne = (req, res) => {
-  
-};
+  // Update a Teacher by the id in the request
+  async function update(req, res) {
+    
+  }
 
-// Update a Teacher by the id in the request
-exports.update = (req, res) => {
-  
-};
+  // Delete a Teacher with the specified id in the request
+  async function deleteOne(req, res) {
+    
+  }
 
-// Delete a Teacher with the specified id in the request
-exports.delete = (req, res) => {
-  
-};
+  // Delete all Teacher from the database.
+  async function deleteAll(req, res) {
+    
+  }
 
-// Delete all Teacher from the database.
-exports.deleteAll = (req, res) => {
-  
-};
+  // Find all finalized Teachers
+  async function findAllFinalized(req, res) {
+    
+  }
 
-// Find all finalized Teachers
-exports.findAllFinalized = (req, res) => {
-  
-};
+  return {
+    create,
+    findAll,
+    findOne,
+    update,
+    deleteOne,
+    deleteAll,
+    findAllFinalized
+  }
+}
