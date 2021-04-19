3d1d6c5aec4e72c0fe566043a461537e71ca5226
ahrayahlabcoimin <ahrayahlabcoimin@gmail.com>
2021-04-18 23:19:52 -0500

    Teacher tests fixes - Fixed bug were unresolved promises caused tests to not terminate - Each Teacher test now starts with a clean Teacher table

--- backend/tests/teacher.routes.test.js
+++ backend/tests/teacher.routes.test.js
@@ -3,13 +3,30 @@
 const request = require('supertest');
 const config = require('./config/test.config');
 const {server, sequelize} = require('../src/server.js')(config);
-const {describe, it, expect, afterAll} = require("@jest/globals");
+const {describe, it, expect, beforeEach, afterAll} = require("@jest/globals");
 
 describe('Post Endpoints', () => {
+    
+    beforeEach(async (done) => {
+        // reload the database
+        await sequelize.query('DELETE FROM Teachers;');
+        await sequelize.query(`
+        INSERT into Teachers (teacherId, firstName,middleName, lastName, tenured, Notes, finalized)
+        VALUES 
+        (0,"Monserrate","D","Brownfield",0,"",1),
+        (1,"Cyndi","W","Gowers",0,"",0), 
+        (2,"Jeannette","E","Dussault",0,"",1),
+        (3,"Golden","W","Freiseis",0,"",0),
+        (4,"Ariane","D","Frix",0,"",1);
+        `);
+        done();
+    });
+
     it("should create a new teacher", async (done) => {
         const res = await request(server)
             .post('/api/teachers/')
             .send({
+                teacherId: 99999,
                 firstName: "Bob",
                 lastName: "Bobber",
                 finalized: false
@@ -20,8 +37,9 @@ describe('Post Endpoints', () => {
         expect(res.body.finalized).toBeFalsy();
         done()
     });
-    afterAll(done => {
-        sequelize.close();
+
+    afterAll(async (done) => {
+        await sequelize.close();
         done()
     })
 });
