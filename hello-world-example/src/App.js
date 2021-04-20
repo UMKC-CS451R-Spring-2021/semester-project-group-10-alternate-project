0cac4e6723adcec968ceead15b123c6ac416c934
kmer102 <jkoni646@gmail.com>
2021-04-20 08:02:59 -0500

    Form updates

--- hello-world-example/src/App.js
+++ hello-world-example/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/Login/Login'
 import Dashboard from './components/Dashboard/Dashboard';
 import Search from './components/TeacherSearch/Search'
 import ProfessorInfo from './components/ProfessorInfo/ProfessorInfo'
+import AddProfessor from './components/ProfessorInfo/AddProfessor'
 
 function setToken(userToken) {
     sessionStorage.setItem('token', JSON.stringify(userToken));
@@ -16,26 +17,21 @@ function getToken() {}
 function App() {
     const [token, setToken] = useState();
 
-    return ( <
-        div className = "App" >
-        <
-        BrowserRouter >
-        <
-        div >
-        <
-        div className = "header" >
+    return ( 
+        <div className = "App" >
+        <BrowserRouter >
+        <div >
+        <div className = "header" >
+        <NavLink activeClassName = "active" to = "/login" > Login ***< /NavLink> 
+        <NavLink activeClassName = "active" to = "/dashboard" > Dashboard ***< /NavLink> <NavLink activeClassName = "active"
+        to = "/search" > Search ***< /NavLink> <
+        NavLink activeClassName = "active"
+        to = "/ProfessorInfo" > ProfessorInfo < /NavLink>
         <
         NavLink activeClassName = "active"
-        to = "/login" > Login < /NavLink> <
-        NavLink activeClassName = "active"
-        to = "/dashboard" > Dashboard < /NavLink> <
-        NavLink activeClassName = "active"
-        to = "/search" > Search < /NavLink > <
-        NavLink activeClassName = "active"
-        to = "/ProfessorInfo" > ProfessorInfo < /NavLink>< /
-        div >
-        <
-        div className = "content" >
+        to = "/AddProfessor" > AddProfessor < /NavLink>
+        </div>
+        <div className = "content" >
         <
         Switch >
         <
@@ -50,7 +46,12 @@ function App() {
         /> <
         Route path = "/ProfessorInfo"
         component = { ProfessorInfo }
-        /> < /
+        /> 
+        <
+        Route path = "/ProfessorInfo"
+        component = { ProfessorInfo }
+        />
+        < /
         Switch > <
         /div> < /
         div > <
--- hello-world-example/src/components/TeacherSearch/Search.js
+++ hello-world-example/src/components/TeacherSearch/Search.js
@@ -4,28 +4,19 @@ import './Search.css'
 export default function Search() {
     return (
         //5 total div tags for the teacher search
-        <
-        div className = "search-wrapper" >
-        <
-        div className = "search-box" >
-        <
-        h6 > Enter teacher ID or Name < /h6> <
-        input type = "text"
-        placeholder = "Search" / >
-        <
-        div >
-        <
-        button type = "submit" > Search < /button> < /
-        div > < /div >
-
-        <
-        p > < h1 > OR < /h1></p >
-
-        <
-        div >
-        <
-        button type = "submit" > Add New Teacher < /button> < /
-        div > <
-        /div>
+        <div className = "allsearch-wrapper" >
+            <div className = "search-box" >
+                <p className="search-instruction"> Enter teacher ID or Name</p>
+                <input className = "search-placeholder" type = "text" placeholder = "Search" / >
+            </div>
+            <div>
+                <button className="search-button" type = "submit" > Search </button> 
+            </div> 
+            
+            <p className="or">OR</p>
+            <div>
+                <button className ="newprof-button" type = "submit" > Add New Teacher </button> 
+            </ div> 
+        </div>
     )
 }
