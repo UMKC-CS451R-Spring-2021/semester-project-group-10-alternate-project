e0f6f819007420bcc5d5eca454948d78f055a47e
Jake Meinershagen <jakemeinershagen@gmail.com>
2021-05-03 22:03:15 -0500

    display teachers on dashboard

--- hello-world-example/src/components/Dashboard/Dashboard.js
+++ hello-world-example/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Dashboard.css'
 import Search from '../TeacherSearch/Search'
-import Semester from '../Semester/Semester'
+import Semester from '../Semester/semester'
 import axios from 'axios'
 import TeacherResource from '../../Resources/TeacherResource'
 
@@ -23,12 +23,25 @@ function Dashboard(props) {
         props.history.push('/semester');
     }
 
+    var output;
     const getAllTeachers = () => {
         (new TeacherResource).get().then(response => {
-            console.log(response);            
+            console.log(response);
+            output = "<p>";
+            var i;
+            for (i = 0; i < response.data.length; i++){
+                output += "ID: " + String(i)
+                    + "       Name: " + String(response.data[i].firstName)
+                    + "       " + String(response.data[i].lastName);
+                output += "       <a src='professorInfo'>Edit</a><br>";
+            }
+            output += "</p>";
+            var doc = document.getElementById("output");
+            doc.innerHTML = output;
+            return output; 
         });
     }
-    
+
     return ( 
         <div className = "container">
             <div className = "top-bar"></div>
@@ -36,12 +49,13 @@ function Dashboard(props) {
                 <div className="dashboard-title"><h1> Dashboard</h1></div>
                 <div><button className="elements" type = "button" onClick = { handleTeacher} > Teacher </button> </div> 
                 <div><button className="elements" type = "button" onClick = { handleSemester}> Semester </button> </div> 
+                <div><button className="elements" type = "button" onClick = { getAllTeachers}> View All Teachers </button> </div> 
                 <div><button className="elements" type = "submit" > Courses </button> </div>
                 <div><input className = 'logout' type = "button" onClick = { handleLogout } value = "Logout" /></div> 
             </div>
             <div className = "top-bar"></div>
+            <div id="output"></div>
         </div>
-
     )
 }
 
